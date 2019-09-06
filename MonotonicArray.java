4900f93ef0519860eb93dac5e2163d0b96a11300
Joey Colombi <32823850+Colombijo@users.noreply.github.com>
2019-09-06 11:58:01 -0700

    Update MonotonicArray.java

--- MonotonicArray.java
+++ MonotonicArray.java
@@ -1,23 +1,23 @@
 
 public class MonotonicArray {
 	
-	public static void main(String[] args) {
-		int[] array = {1, 2, 2, 3};
-		System.out.println(isMonotonic(array));
-		
+    public static void main(String[] args) {
+    	int[] array = {1, 2, 2, 3};
+	System.out.println(isMonotonic(array));
+    }
+
+    public static boolean isMonotonic(int[] A) {
+	boolean decreasing = true;
+	boolean increasing = true;
+	for (int i = 1; i < A.length; i++) {
+	    if (A[i] > A[i-1]) {
+		decreasing = false;
+	    } else if (A[i] < A[i-1]) {
+		increasing = false;
+	    }
 	}
 	
-	public static boolean isMonotonic(int[] A) {
-		boolean decreasing = true;
-        boolean increasing = true;
-        for (int i = 1; i < A.length; i++) {
-            if (A[i] > A[i-1]) {
-                decreasing = false;
-            } else if (A[i] < A[i-1]) {
-                increasing = false;
-            }
-        }
-        return (decreasing || increasing);
+	return (decreasing || increasing);
     }
 
 }
